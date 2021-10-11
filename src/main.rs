87815bb8976be1d13277668de1ef73186f0e1836
Kaonan Micadei <k.micadei@gmail.com>
2021-10-11 20:30:38 +0200

    RK4 implementation

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+mod rk4;
+
+fn main() {
+    use rk4::rk4::RK4;
+
+    let f = |x, y| { -2.0*x*y };
+    let mut p: RK4<f32> = RK4 { x: 0.0, y: 1.0, step: 0.1, f };
+    
+    for i in 1..=20 {
+        println!("{}, {:?}", i, p.next().unwrap());
+    }
+}
---
+++ src/rk4/mod.rs
@@ -0,0 +1,30 @@
+pub mod rk4 {
+    use num::Float;
+
+    pub struct RK4<T> {
+        pub x: T,
+        pub y: T,
+        pub step: T,
+        pub f: fn(T,T) -> T
+    }
+
+    impl<T> Iterator for RK4<T>
+        where T: Float
+    {
+        type Item = T;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            let c2: T = num::cast(2).unwrap(); 
+            let c6: T = num::cast(6).unwrap();
+
+            let k1 = self.step * (self.f)(self.x, self.y);
+            let k2 = self.step * (self.f)(self.x + self.step/c2, self.y + k1/c2);
+            let k3 = self.step * (self.f)(self.x + self.step/c2, self.y + k2/c2);
+            let k4 = self.step * (self.f)(self.x + self.step, self.y + k3);
+            self.y = self.y + (k1 + c2*k2 + c2*k3 + k4)/c6;
+            self.x = self.x + self.step;
+            
+            Some(self.y)
+        }
+    }
+}
